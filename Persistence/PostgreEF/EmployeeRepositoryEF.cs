69bc6f242216ffce2045c506ffa4a1ad90e54da0
Jonathan M Borges <jonathan.m.borges@gmail.com>
2020-06-16 20:31:14 -0300

    parcial

---
+++ Persistence/PostgreEF/EmployeeRepositoryEF.cs
@@ -0,0 +1,43 @@
+﻿using RestApiEmployees.Domain.Models;
+using RestApiEmployees.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestApiEmployees.Persistence.PostgreEF
+{
+    public class EmployeeRepositoryEF : IEmployeeRepository
+    {
+        private readonly EmployeesDbContext context;
+
+        public EmployeeRepositoryEF(EmployeesDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> ListAll()
+        {
+            return context.Employees.ToList();
+        }
+
+        public bool Update(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
